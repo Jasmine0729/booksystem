d7d6d964dae93291d795e9eed069033d2bba8fdb
Jasmine0729 <ljr19950729.com>
2015-11-14 00:20:40 +0800

    third test

--- HW/HW/urls.py
+++ HW/HW/urls.py
@@ -7,7 +7,7 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'HW.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-    #url(r'^AddBook/','blog.views.add_book'),
+    url(r'^AddBook/','blog.views.add_book'),
     url(r'^AddAuthor/','blog.views.add_author'),
     url(r'^AllInformation/','blog.views.all_information'),
     url(r'^DeleteInformation/','blog.views.delete_information'),
